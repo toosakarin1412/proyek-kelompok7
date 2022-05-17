2a6e40ec42128cbab009870266c6d8fe00f67c23
NADIAMUQARRAMAH <nadia2003aja@gmail.com>
2022-05-17 22:08:29 +0700

    tambahan pada registrasi.c

--- registrasi.c
+++ registrasi.c
@@ -8,11 +8,14 @@ void write2bin(peserta data){
 }
 
 int main(int argc, char *argv[]){
+    peserta data;
     if(argc != 3){
         printf("Silahkan masukkan username dan password\n");
         printf("./reg [username] [password\n");
         return EXIT_FAILURE;
     }
 
+    write2bin(data);
+    printf("Registrasi berhasil...\n");
     return EXIT_SUCCESS;
 }
